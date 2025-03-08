bef253173391615c78befb2a2c9ec972f1b2ebdc
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-03-08 15:50:00 +0530

    Calling getAllCar Api from backend

--- Car_Rental_Frontend/src/app/modules/customer/components/customer-dashboard/customer-dashboard.component.ts
+++ Car_Rental_Frontend/src/app/modules/customer/components/customer-dashboard/customer-dashboard.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
-  selector: 'app-customer-dashboard',
-  templateUrl: './customer-dashboard.component.html',
-  styleUrls: ['./customer-dashboard.component.scss']
+    selector: 'app-customer-dashboard',
+    templateUrl: './customer-dashboard.component.html',
+    styleUrls: ['./customer-dashboard.component.scss']
 })
 export class CustomerDashboardComponent {
 
+    cars: any = [];
+
+    constructor(
+        private customerService: CustomerService,
+        private message: NzMessageService
+    ) {
+        this.getAllCars();
+    }
+
+    getAllCars() {
+        this.customerService.getAllCars().subscribe((res) => {
+            console.log(res)
+            res.forEach((element: any) => {
+                element.processedImg = 'data:image/jpeg;base64,' + element.returnedImage;
+                this.cars.push(element);
+            });
+        })
+    }
 }
--- Car_Rental_Frontend/src/app/modules/customer/customer.module.ts
+++ Car_Rental_Frontend/src/app/modules/customer/customer.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerDashboardComponent } from './components/customer-dashboard/customer-dashboard.component';
+import { NgZorroImportsModules } from 'src/app/NgZorroImportsModules';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { CustomerDashboardComponent } from './components/customer-dashboard/cust
   ],
   imports: [
     CommonModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    NgZorroImportsModules
   ]
 })
 export class CustomerModule { }
