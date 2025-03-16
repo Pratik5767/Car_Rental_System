ff7637fa3857078f37a2cafc173da43fdb41f6ec
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-03-16 15:25:47 +0530

    implements search car functionality for customer

--- Car_Rental_Frontend/src/app/modules/customer/components/search-car/search-car.component.ts
+++ Car_Rental_Frontend/src/app/modules/customer/components/search-car/search-car.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
-  selector: 'app-search-car',
-  templateUrl: './search-car.component.html',
-  styleUrls: ['./search-car.component.scss']
+    selector: 'app-search-car',
+    templateUrl: './search-car.component.html',
+    styleUrls: ['./search-car.component.scss']
 })
 export class SearchCarComponent {
+
+    cars: any = [];
+
+    isSpinning: boolean = false;
+    validateForm!: FormGroup;
+    listOfOption: Array<{ label: string; value: string }> = [];
+    listOfBrands = ["BMW", "AUDI", "FERRARI", "TESLA", "TOYOTA", "HONDA", "FORD", "NISSAN", "HYUNDIA", "LEXUS", "KIA"];
+    listOfType = ["PETROL", "HYBRID", "DIESEL", "ELECTRIC", "CNG"];
+    listOfColor = ["RED", "WHITE", "BLUE", "BLACK", "ORANGE", "GREY", "SILVER"];
+    listOfTransmission = ["MANUAL", "AUTOMATIC"];
+
+    constructor(private fb: FormBuilder, private customerService: CustomerService) {
+        this.validateForm = this.fb.group({
+            brand: [null],
+            type: [null],
+            color: [null],
+            transmission: [null],
+        })
+    }
+
+    searchCar() {
+        this.isSpinning = true;
+        this.customerService.searchCar(this.validateForm.value).subscribe((res) => {
+            this.isSpinning = false;
+            console.log(res);
+            res.carDtoList.forEach((element: any) => {
+                element.processedImg = 'data:image/jpeg;base64,' + element.returnedImage;
+                this.cars.push(element);
+            });
+        })
+    }
 
 }
